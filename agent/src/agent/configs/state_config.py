27b6faf36b7498ea62066305f2b971289235e32c
Bittu Kumar <bitweb24@gmail.com>
2025-11-04 22:05:48 +0530

    rm default chat_id

--- agent/src/agent/configs/state_config.py
+++ agent/src/agent/configs/state_config.py
@@ -1,5 +1,5 @@
 from langchain_core.runnables import RunnableConfig
 
 
-def state_config(chat_id: str = "S2706406N1910241200174"):
+def state_config(chat_id: str):
     return RunnableConfig(recursion_limit=1000, configurable={"thread_id": chat_id})
