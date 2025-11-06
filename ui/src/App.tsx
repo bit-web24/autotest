117a78ef9d24d2ead3a7ed4ae3efe85f55f45047
Bittu Kumar <bitweb24@gmail.com>
2025-11-06 21:07:05 +0530

    display sse to frontend

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Chat from "./components/Chat";
 import Home from "./components/Home";
 import Dashboard from "./components/Dashboard";
+import Stream from "./components/Stream";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/chat" element={<Chat />} />
+        <Route path="/stream" element={<Stream />} />
         <Route path="*" element={<h1>404 - Page Not Found</h1>} />
       </Routes>
     </Router>
